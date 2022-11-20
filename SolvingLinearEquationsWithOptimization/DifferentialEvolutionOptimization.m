59d5ef05694929e88b860ce468c143fad0ef02e2
Jorge Herrada <jorge.herrada.28@gmail.com>
2022-11-20 02:25:37 -0600

    Added 4th system and disp results

--- SolvingLinearEquationsWithOptimization/DifferentialEvolutionOptimization.m
+++ SolvingLinearEquationsWithOptimization/DifferentialEvolutionOptimization.m
@@ -1,4 +1,4 @@
-% Diferential Evolution using Penalty Function
+% Differential Evolution 
 % DE/best/2/bin
 
 % Jorge Herrada
@@ -6,41 +6,39 @@
 clear all
 close all
 clc
-plotting = 0; % need the plots? Yes(1), No(0)
+
+
+opc = 4; % seleccionar sistema
 
 % definicion del sistemas de ecuaciones
-opc = 3;
-
 if opc == 1
     A = [2 3; 5 1]; % matriz de escalares
     b = [7;11];     % vector de resultado
-    m = numel(b);   % dimension
+    m = numel(b);   % # de ecuaciones
     xl = [-5 -5]';  % lower value for x,y coordinates
     xu = [5 5]';    % upper value for x,y coordinates
 elseif opc == 2
     A = [2 3 4; 1 2 3; 5 1 0; 3 4 1]; % matriz de escalares
     b = [19;13;11;13];     % vector de resultado
-    m = numel(b);   % dimension
+    m = numel(b);   % # de ecuaciones
     xl = [-5 -5 -5]';  % lower value for x,y coordinates
     xu = [5 5 5]';    % upper value for x,y coordinates
 elseif opc == 3
     A = [2 3; 5 4; 2 5; 4 1; 0.5 0.5]; % matriz de escalares
     b = [-5;5;-15;15;0];     % vector de resultado
-    m = numel(b);   % dimension
+    m = numel(b);   % # de ecuaciones
     xl = [-5 -5]';  % lower value for x,y coordinates
     xu = [5 5]';    % upper value for x,y coordinates
 elseif opc == 4
-    A = [2 3; 5 1]; % matriz de escalares
-    b = [7;11];     % vector de resultado
-    m = numel(b);   % dimension
+    A = [3 1 2 2;4 2 2 3;4 3 7 5;1 6 9 8]; % matriz de escalares
+    b = [3;5;-3;5];     % vector de resultado
+    m = numel(b);       % # de ecuaciones
+    xl = [-5 -5 -5 -5]';   % lower value for x,y coordinates
+    xu = [5 5 5 5]';      % upper value for x,y coordinates
 end
 
 % Function objetivo basada en el error
 f = @(x) (1/(2*m))*sum((b - A*x).^2);
-
-% Funciones de penalizacion
-% fp = @(x,xl,xu) f(x(1),x(2)) + 1000*Penalty1(x,xl,xu);
-% fp = @(x,xl,xu) f(x(1),x(2)) + 1000*Penalty2(x,xl,xu);
 
 % definicion de parametros iniciales
 
@@ -112,80 +110,17 @@ for g=1:G
 
     end
 
-    
-    
-
-    % plot de toda la poblacion de la generacion
-%     Plot_Contour(f,x,xl,xu);
     % guarda indice del mejor valor de la generacion
-    [~,xb] = min(fitness);
-    f_plot(g) = fitness(xb);
+%     [~,xb] = min(fitness);
+%     f_plot(g) = fitness(xb);
 end
 
-
-
+[~,xb] = min(fitness);
 
 %  *****************Display results************************
-
+disp(['Mejor valor para x: ']);
 x(:,xb)     % mejor valor para x1,x2,...,xn
+disp(['Error: ']);
 f(x(:,xb))  % error
+disp(['Comprobación de Resultado: ']);
 A*x(:,xb)   % comprobacion de resultado
-
-%*******************Plotting*******************************
-if plotting
-    Plot_Contour(f,x(:,xb),xl,xu);
-    figure
-    Plot_Surf(f,x(:,xb),xl,xu);
-    
-    figure
-    hold on
-    grid on
-    plot(f_plot,'b-','LineWidth',2)
-    title("Gráfica de Convergencia")
-    xlabel("Iteraciones")
-    ylabel("f(x)")
-end
-
-%% Funcion de penalizacion 2 con exponentes
-function z = Penalty2(x,xl,xu)
-    z = 0;
-    D = numel(xl);
-    
-    for j=1:D
-        
-        if xl(j)<x(j)
-            z = z + 0;
-        else
-            z = z + (xl(j)-x(j))^2;
-        end
-
-        if x(j)<xu(j)
-            z = z + 0;
-        else
-            z = z + (xu(j)-x(j))^2;
-        end
-    end
-
-end
-
-
-
-%% Funcion de penalizacion 1 con suma
-function z = Penalty1(x,xl,xu)
-    z = 0;
-    D = numel(xl);
-    
-    for j=1:D
-        
-        if xl(j)<x(j) && x(j)<xu(j)
-            z = z + 0;
-        else
-            z = z + 1;
-        end
-    end
-
-end
-
-
-
-
